0d5428b698895e8ada92d81af661a84917e7ae87
SeungyoupBaek <bsy8220@naver.com>
2014-01-06 14:24:59 +0900

    에러가 납니다.
    에러가 납니다.

--- 140p/ViewController.m
+++ 140p/ViewController.m
@@ -9,10 +9,51 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+-(void)stopTimer;
+-(void)startTimer;
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSTimer *timer;
+    BOOL timerWorking;
+}
+
+// 각 1초마다 1분씩감소
+-(void)tickDown:(NSTimer *)timer{
+    self.datePicker.countDownDuration = self.datePicker.countDownDuration - 60;
+    // 1분이내면 정지
+    if(self.datePicker.countDownDuration <= 60){
+        NSLog(@"Done!");
+        [self stopTimer];
+    }
+}
+
+// 타이머 시작
+-(void)startTimer{
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tickDown:) userInfo:Nil repeats:YES];
+}
+
+// 타이머 정지
+-(void)stopTimer{
+    [timer invalidate];
+    timer = nil;
+    timerWorking = NO;
+}
+
+-(IBAction)toggleTimer:(UIButton *)sender{
+    //버튼토클설정
+    NSString *title = timerWorking ? @"Start" : @"Stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking = !timerWorking;
+    if (timerWorking) {
+        [self startTimer];
+    }
+    else{
+        [self stopTimer];
+    }
+}
+
 
 - (void)viewDidLoad
 {
